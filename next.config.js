cebbbd3d611ebd0425970c66306438f07ef124d1
LeeJeongWon <ljw092601@naver.com>
2026-06-11 20:52:43 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,2 +1,9 @@
-const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // 깃허브 페이지는 자체 이미지 최적화를 지원하지 않으므로 이 옵션을 반드시 켜야 합니다.
+  },
+};
+
+module.exports = nextConfig;
